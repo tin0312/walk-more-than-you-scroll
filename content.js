912a8f48af1a507739e61d69cffae7fb4a7ca45d
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-08-01 12:51:17 -0400

    Create the live scroll counter at the right corner of the website

--- content.js
+++ content.js
@@ -1,13 +1,33 @@
-const initialScrollPosition = window.scrollY;
-let lastScrollPosition = initialScrollPosition;
+const liveCounter = document.createElement('div');
+liveCounter.classList.add('scroll-counter');
+
+document.body.appendChild(liveCounter);
 
 const websiteDomain = window.location.hostname;
+
+const initialScrollPosition = window.scrollY;
+let lastScrollPosition = initialScrollPosition;
+let scrollDistance = 0;
+
+chrome.storage.local.get('scrollStats', ({ scrollStats }) => {
+	if (!scrollStats) return;
+	scrollDistance = scrollStats[websiteDomain]?.distance || 0;
+	updateLiveCounter(scrollDistance);
+});
 
 const updateScrollDistance = () => {
 	const scrollPosition = window.scrollY;
 	const distance = Math.abs(scrollPosition - lastScrollPosition);
 	lastScrollPosition = scrollPosition;
 	saveDistance(websiteDomain, distance);
+
+	scrollDistance += distance;
+	updateLiveCounter(scrollDistance);
+};
+
+const updateLiveCounter = (distance) => {
+	const distanceCM = pixelToCm(distance).toFixed(0);
+	liveCounter.innerHTML = `${distanceCM} cm `;
 };
 
 window.addEventListener('scroll', updateScrollDistance);
@@ -53,3 +73,11 @@ const saveDistance = (() => {
 })();
 
 const isContextInvalidated = () => !chrome.runtime?.id;
+
+function pixelToCm(px) {
+	const DPI = 96; // Assumed dpi value for desktops, there is no way to correctly determine screen DPI
+
+	const lengthInInch = px / DPI;
+	const INCH_TO_CM = 2.54;
+	return lengthInInch * INCH_TO_CM;
+}
