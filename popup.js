dbbee67d8b6501a6379e442fc032822229fb5c2e
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-08-01 12:51:17 -0400

    Create the live scroll counter at the right corner of the website

--- popup.js
+++ popup.js
@@ -1,18 +1,17 @@
 // Initialize button with users' preferred color
-const counter = document.getElementById('distance');
+const counter = document.getElementById("distance");
 
-chrome.storage.local.get('totalDistance', ({ totalDistance }) => {
-	const distanceCM = pixelToCm(totalDistance).toFixed(0);
-	counter.innerText = `Scroll distance: ${distanceCM} cm`;
+chrome.storage.local.get("totalDistance", ({ totalDistance }) => {
+  const distanceCM = pixelToCm(totalDistance).toFixed(0);
+  counter.innerText = `Scroll distance: ${distanceCM} cm`;
 });
-
 /**
  * Estimate length in pixel to centimeter
  */
 function pixelToCm(px) {
-	const DPI = 96; // Assumed dpi value for desktops, there is no way to correctly determine screen DPI
-	const INCH_TO_CM = 2.54;
+  const DPI = 96; // Assumed dpi value for desktops, there is no way to correctly determine screen DPI
+  const INCH_TO_CM = 2.54;
 
-	const lengthInInch = px / DPI;
-	return lengthInInch * INCH_TO_CM;
+  const lengthInInch = px / DPI;
+  return lengthInInch * INCH_TO_CM;
 }
