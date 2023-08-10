d172290fe95670828c93ae616323ab6c8efa0d85
DukeManh <manhducdkcb@gmail.com>
2023-08-09 22:37:05 -0400

    Display the scroll distances by domain in popup

--- popup.js
+++ popup.js
@@ -1,8 +1,33 @@
 /* global pixelToMeter */
-// Initialize button with users' preferred color
-const counter = document.getElementById('distance');
 
-chrome.storage.local.get('totalDistance', ({ totalDistance }) => {
-  const distanceMeter = pixelToMeter(totalDistance).toFixed(2);
-  counter.innerText = `Scroll distance: ${distanceMeter} m`;
+const counter_elem = $('#distance');
+const stats_elem = $('#stats');
+
+chrome.storage.local.get(['totalDistance', 'scrollStats'], ({ totalDistance, scrollStats }) => {
+  updateDistance(totalDistance);
+
+  // list the scroll distance for each domain
+  if (!scrollStats || !stats_elem) return;
+
+  const stats = Object.entries(scrollStats);
+  const top3 = stats.sort(([, { distance: a }], [, { distance: b }]) => b - a).slice(0, 3);
+  updateScrollDistances(top3);
 });
+
+function updateScrollDistances(scrollDistances) {
+  scrollDistances.foreach(([domain, { distance }]) => {
+    const distanceInMeter = pixelToMeter(distance).toFixed(2);
+    const text = `${domain}: ${distanceInMeter} cm`;
+    const li = $('<li></li>');
+    li.text(text);
+    // list.appendChild(li);
+    stats_elem.append(li);
+  });
+}
+
+function updateDistance(distance) {
+  if (counter_elem) {
+    const distanceMeter = pixelToMeter(distance).toFixed(2);
+    counter_elem.text(`Scroll distance: ${distanceMeter} m`);
+  }
+}
