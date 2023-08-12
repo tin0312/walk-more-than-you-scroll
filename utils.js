471aa9ae2e9bc877038d2ac7b3b8fff72ab80679
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-08-12 14:36:38 -0400

    Converting distance from cm to m

---
+++ utils.js
@@ -0,0 +1,7 @@
+function pixelToMeter(px) {
+  const DPI = 96; // Assumed dpi value for desktops, there is no way to correctly determine screen DPI
+  const INCH_PER_METER = 39.37; // 1 inch = 39.37 cm
+  const lengthInInch = px / DPI;
+  const lengthInMeter = lengthInInch / INCH_PER_METER;
+  return lengthInMeter;
+}
