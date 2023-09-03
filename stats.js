e3836d520bb5bcc75d1d7838433405b8c42808b4
Duc Manh <51073515+DukeManh@users.noreply.github.com>
2023-09-03 16:22:01 -0400

    Chart: Highlight today's scroll distance and allowing selecting day (#23)
    * Allow selecting day in the summary chart
    * Update popup header and stats section when selecting a day
    * update stats.js to accept Date type instead of date string typ

--- stats.js
+++ stats.js
@@ -5,23 +5,23 @@
  */
 
 const stats = {
-  getTotalDistance: async (date = new Date().toLocaleDateString()) => {
+  getTotalDistance: async (date = new Date()) => {
     const dayStats = await stats.getDayStats(date);
     return dayStats.totalDistance;
   },
 
-  getDomainDistance: async (domain, date = new Date().toLocaleDateString()) => {
+  getDomainDistance: async (domain, date = new Date()) => {
     const dayStats = await stats.getDayStats(date);
     return dayStats.domains[domain]?.distance || 0;
   },
 
   getTodayStats: async () => {
-    const date = new Date().toLocaleDateString();
+    const date = new Date();
     return stats.getDayStats(date);
   },
 
   updateDistance: async (domain, distance) => {
-    const date = new Date().toLocaleDateString();
+    const date = new Date();
     const dayStats = await stats.getDayStats(date);
 
     dayStats.totalDistance += distance;
@@ -55,23 +55,25 @@ const stats = {
     return week.reverse();
   },
 
-  /** @type {(date: string) => Promise<DayStats>} */
+  /** @type {(date: Date) => Promise<DayStats>} */
   getDayStats: async (date) => {
     const { days } = await chrome.storage.local.get(['days']);
-    if (!days[date]) {
-      days[date] = {
+    const dateString = date.toLocaleDateString();
+    if (!days[dateString]) {
+      days[dateString] = {
         totalDistance: 0,
         domains: {},
       };
       await chrome.storage.local.set({ days });
     }
-    return days[date];
+    return days[dateString];
   },
 
-  /**  @type {(date: string, dayStats: DayStats) => Promise<void>} */
+  /**  @type {(date: Date, dayStats: DayStats) => Promise<void>} */
   updateDayStats: async (date, dayStats) => {
     const { days } = await chrome.storage.local.get(['days']);
-    days[date] = dayStats;
+    const dateString = date.toLocaleDateString();
+    days[dateString] = dayStats;
     return chrome.storage.local.set({ days });
   },
 };
